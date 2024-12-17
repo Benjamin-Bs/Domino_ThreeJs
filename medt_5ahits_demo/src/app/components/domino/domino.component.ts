b87f02fa07eef4dde8943c44f4a6f040a569fc0d
Benjamin Suljevic <bsuljevic1809@gmail.com>
2024-12-17 12:06:23 +0100

    changes to add the donimo component to page 2

--- medt_5ahits_demo/src/app/components/domino/domino.component.ts
+++ medt_5ahits_demo/src/app/components/domino/domino.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 
 @Component({
   selector: 'app-domino',
   templateUrl: './domino.component.html',
   styleUrls: ['./domino.component.scss'],
 })
-export class DominoComponent  implements OnInit {
+export class DominoComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('domino') canvas!: ElementRef<HTMLCanvasElement>;
+
+  scene!: Scene;
+  camera!: PerspectiveCamera;
+  renderer!: WebGLRenderer;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
+  ngAfterViewInit(): void {
+    this.initScene();
+  }
+
+  private initScene(): void {
+    this.scene = new Scene;
+    this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+    this.renderer = new WebGLRenderer({ canvas: this.canvas.nativeElement, antialias: true })
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.shadowMap.enabled = true;
+  }
+
+
+  
 }
